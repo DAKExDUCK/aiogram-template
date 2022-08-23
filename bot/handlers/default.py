6b48d3e47b1aaa7703b805af8b71a46a3b729d5d
DAKExDUCK <arsengabdulin228@gmail.com>
2022-08-23 20:43:33 +0600

    small fixes

--- bot/handlers/default.py
+++ bot/handlers/default.py
@@ -3,7 +3,6 @@ import random
 
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from bot.functions.rights import admin_list
 from bot.handlers.logger import logger, print_msg
 from bot.keyboards.default import add_delete_button
@@ -14,19 +13,13 @@ from bot.objects.chats import chat_store
 async def start(message: types.Message, state: FSMContext):
     text = "Start message"
     await message.reply(text, reply_markup=add_delete_button())
-
-
-@print_msg
-async def stop(message: types.Message, state: FSMContext):
-    text = "Stop message"
-    await message.reply(text)
     await state.finish()
 
 
 @print_msg
 async def help(message: types.Message):
     text = "Help message"
-    await message.reply(text)
+    await message.reply(text, reply_markup=add_delete_button())
 
 
 @print_msg
@@ -45,19 +38,18 @@ async def msg_to_admin(message: types.Message):
 
 
 async def delete_msg(query: types.CallbackQuery):
-    await query.answer()
     try:
         await query.bot.delete_message(query.message.chat.id, query.message.message_id)
         if query.message.reply_to_message:
             await query.bot.delete_message(query.message.chat.id, query.message.reply_to_message.message_id)
+        await query.answer()
     except Exception as exc:
         logger.error(exc)
-        await query.answer("Error with deleting, message is too old")
+        await query.answer("Error")
 
 
 def register_handlers_default(dp: Dispatcher):
     dp.register_message_handler(start, commands="start", state="*")
-    dp.register_message_handler(stop, commands="stop", state="*")
     dp.register_message_handler(help, commands="help", state="*")
     dp.register_message_handler(msg_to_admin, commands="msg_to_admin", state="*")
 
